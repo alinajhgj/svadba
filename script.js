db68433dba4035c01a24469cf45dfff47094b1ab
alina <136694750+alinajhgj@users.noreply.github.com>
2025-04-11 23:30:43 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("confirmationForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const attend = document.getElementById("attend").value;
-
-    alert(`${name}, сіздің тойға қатысуыңыз: ${attend === 'yes' ? 'Иә' : 'Жоқ'}`);
-});
