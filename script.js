b1abcb3dc801cf18f1726f9c9a75878ecef34507
alina <136694750+alinajhgj@users.noreply.github.com>
2025-04-11 23:50:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const form = document.getElementById('rsvpForm');
-const successMsg = document.getElementById('successMsg');
+document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const params = new URLSearchParams(formData);
+    var name = document.getElementById('name').value;
+    var attendance = document.getElementById('attendance').value;
 
-  fetch('https://script.google.com/macros/s/AKfycbyOijfnmi9v_EK4VnqKOHqmEqmbjzYyhDmhRGwi2owHMkUzhu3qJaiP86bbL9KIIlPi/exec', {
-    method: 'POST',
-    body: params
-  })
-    .then(() => {
-      form.reset();
-      successMsg.style.display = 'block';
-    })
-    .catch(err => alert("Қате болды: " + err));
+    var response = {
+        name: name,
+        attendance: attendance
+    };
+
+    sendRSVP(response);
 });
+
+function sendRSVP(response) {
+    var scriptURL = 'https://script.google.com/macros/s/AKfycbyOijfnmi9v_EK4VnqKOHqmEqmbjzYyhDmhRGwi2owHMkUzhu3qJaiP86bbL9KIIlPi/exec';
+    
+    fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify(response),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => alert('Спасибо за ответ!'))
+    .catch(error => alert('Ошибка отправки данных!'));
+}
